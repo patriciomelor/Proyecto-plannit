9a095403b135d6a6fc0207b38cf6963b51246eb5
gringows <esteban.ams95@gmail.com>
2020-09-12 10:11:22 -0300

    localhost redirects to login, a fucking ugly login

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+
+    path('', views.RootView.as_view(), name='screen'),
     path('index/', views.IndexView.as_view(), name='index'),
     path('escritorio/', views.EscritorioView.as_view(), name='Escritorio'),
     path('paneldecarga/',views.PdcView.as_view(), name='PanelCarga'),
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView, RedirectView, View
 # Create your views here.
 
 
 class RootView(RedirectView):
-    pattern_name = 'login'
+    pattern_name = 'account_login'
 
 class IndexView(TemplateView):
     template_name = "index-base.html"
