0a83dd864b8b1d1d4d38755d0f9cd857a04f90ad
gringows <esteban.ams95@gmail.com>
2020-09-12 09:56:58 -0300

    try

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView, View
 # Create your views here.
+
+
+class RootView(RedirectView):
+    pattern_name = 'login'
 
 class IndexView(TemplateView):
     template_name = "index-base.html"
+
