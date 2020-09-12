e89ce63516cd2647e5d513e7152e9347f5cf9e6d
gringows <esteban.ams95@gmail.com>
2020-09-12 04:50:44 -0300

    added the collectstatics

--- src/dmp/settings.py
+++ src/dmp/settings.py
@@ -136,5 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
