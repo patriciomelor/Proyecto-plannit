d29a55be17654614d25085a8e11e71c848969e39
stodcl <pmelor@stod.cl>
2020-09-21 17:22:51 -0300

    ajustes varios

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -4,3 +4,9 @@ from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index-base.html"
+
+class EscritorioView(TemplateView):
+    template_name = "administrador/Escritorio/dash.html"
+
+class PdcView(TemplateView):
+    template_name = "administrador/PaneldeCarga/pdc.html"
