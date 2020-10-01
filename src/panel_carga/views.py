a6447bebf3d9b0b8dd40433e5144002e949a38d5
gringows <esteban.ams95@gmail.com>
2020-10-01 01:20:41 -0300

    seleccionar proyecto a trabajar

--- src/panel_carga/views.py
+++ src/panel_carga/views.py
@@ -7,10 +7,10 @@ from django.views.generic.base import TemplateView, RedirectView, View
 from django.urls import reverse_lazy
 from .models import Proyecto, Documento
 from .forms import ProyectoForm, DocumentoForm, ProyectoSelectForm
+from tools.views import ProyectoSeleccionadoMixin
 # Create your views here.
 
-
-class ProyectoSelectView(LoginRequiredMixin, FormView):
+class ProyectoSelectView(LoginRequiredMixin, SuccessMessageMixin, FormView):
     success_message = 'Bienvenido!'
     success_url = reverse_lazy('index')
     template_name = 'panel_carga/list-proyecto.html'
@@ -21,21 +21,24 @@ class ProyectoSelectView(LoginRequiredMixin, FormView):
         self.request.session['proyecto'] = form.cleaned_data['proyectos'].pk
         return super().form_valid(form)
 
+class ProyectoMixin(SuccessMessageMixin, ProyectoSeleccionadoMixin):
+    model = Proyecto
+
 class ProyectoList(ListView):
     queryset = Proyecto.objects.all()
     template_name = 'panel_carga/list-proyecto.html'
     context_object_name = 'proyectos'
 
-class CreateProyecto(CreateView):
+class CreateProyecto(ProyectoMixin, CreateView):
     form_class = ProyectoForm
     template_name = 'panel_carga/create-proyecto.html'
     success_url = reverse_lazy("index")
 
-class DetailProyecto(DetailView):
+class DetailProyecto(ProyectoMixin, DetailView):
     model = Proyecto
     template_name = 'panel_carga/detail-proyecto.html'
 
-class DocumentoList(ListView):
+class DocumentoList(ProyectoMixin, ListView):
     model = Documento
     context_object_name = 'documentos'
     template_name='administrador/PaneldeCarga/pdc.html'
--- src/tools/views.py
+++ src/tools/views.py
@@ -2,17 +2,27 @@ from datetime import date, datetime
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic import DeleteView
 from django.views.generic.base import ContextMixin
+from panel_carga.models import Proyecto
 
-# class ProyectoSeleccionadoMixin(LoginRequiredMixin, ContextMixin):
-#     proyecto = None
+class ProyectoSeleccionadoMixin(LoginRequiredMixin, ContextMixin):
+    proyecto = None
     
-#     def dispatch(self, request, *args, **kwargs):
-#         if not request.user.is_authenticated:
-#             return self.handle_no_permission()
-#         if not request.sessions.get('proyecto', None):
-#             return HttpResponseRedirect(reverse_lazy('proyecto-select'))
-#         return super().dispatch(request, *args, **kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        if not request.session.get('proyecto', None):
+            return HttpResponseRedirect(reverse_lazy('proyecto-select'))
+        return super().dispatch(request, *args, **kwargs)
+        proyecto_id = request.session.get('proyecto')
+        self.proyecto = Proyecto.objects.get(pk=proyecto_id)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['proyecto'] = self.proyecto
+        return context
+    
     
