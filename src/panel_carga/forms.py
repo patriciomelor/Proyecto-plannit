2ed0e69280cb4c2db4a6e8bd548e6ceaa0779aa5
stodcl <pmelor@stod.cl>
2020-10-06 20:39:05 -0300

    cripy form

--- src/panel_carga/forms.py
+++ src/panel_carga/forms.py
@@ -1,14 +1,18 @@
 from django import forms
 import django.forms.widgets
 from .models import Proyecto, Documento, Revision
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class ProyectoSelectForm(forms.Form):
     proyectos =  forms.ModelChoiceField(queryset=Proyecto.objects.all())
 
 class ProyectoForm(forms.ModelForm):
+    nombre = forms.CharField( max_length=50, required=True)
+    fecha_inicio = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
     class Meta:
         model = Proyecto
-        fields = '__all__'
+        fields = ['encargado']
 
 class DocumentoForm(forms.ModelForm):
     class Meta:
