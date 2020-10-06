664cef0fb051479d3e5aeb63e728345b3c68a467
gringows <esteban.ams95@gmail.com>
2020-10-06 20:09:21 -0300

    migraciones aplicadas

--- src/panel_carga/models.py
+++ src/panel_carga/models.py
@@ -16,12 +16,12 @@ class Proyecto(models.Model):
         return self.nombre
    
 class Revision(models.Model):
-
     tipo = models.IntegerField(choices=TYPES_REVISION, verbose_name="Tipo_Revision", null=False, default=1)
     estado_cliente = models.IntegerField(choices=ESTADOS_CLIENTE, default=1)
     estado_contratista = models.IntegerField(choices=ESTADO_CONTRATISTA, default=1)
     emitida_para = models.TextField(verbose_name="Emitida_para")
     fecha = models.DateTimeField(verbose_name="Fecha", editable=False, null=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.tipo
@@ -30,11 +30,12 @@ class Documento(models.Model):
     nombre = models.CharField(verbose_name="Nombre del Documento", max_length=100, null=False)
     especialidad = models.CharField(verbose_name="Especialidad", max_length=100, null=False)
     descripcion = models.TextField(verbose_name="Descripción", blank=True)
-    num_documento = models.CharField(verbose_name="Número de Documento", max_length=100, null=False)
+    num_documento = models.CharField(verbose_name="Código de Documento", max_length=100, null=False)
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE, null=True)
     emision = models.ForeignKey(Revision, on_delete=models.CASCADE, blank=True, null=True)
     tipo = models.CharField(verbose_name="Típo de Documento", max_length=50, null=False)
     archivo = models.FileField(upload_to="proyecto/documentos/", null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.nombre
