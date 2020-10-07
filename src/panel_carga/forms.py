8a2c31a0a3f20c74afae3e3f35d180a461a9d823
stodcl <pmelor@stod.cl>
2020-10-07 00:45:16 -0300

    pana qla rara la wea ! GRINGO TE AMO <3

--- src/panel_carga/forms.py
+++ src/panel_carga/forms.py
@@ -10,16 +10,33 @@ class ProyectoSelectForm(forms.Form):
 class ProyectoForm(forms.ModelForm):
     nombre = forms.CharField( max_length=50, required=True)
     fecha_inicio = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+    fecha_termino = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+    descripcion = forms.CharField(widget=forms.Textarea, max_length=100, required=True)
     class Meta:
         model = Proyecto
         fields = ['encargado']
 
 class DocumentoForm(forms.ModelForm):
+
+    nombre = forms.CharField( max_length=50, required=True)
+    especialidad = forms.CharField( max_length=50, required=True)
+    descripcion = forms.CharField(widget=forms.Textarea, max_length=150, required=True)
+    num_documento = forms.CharField( max_length=10, required=True)
+    tipo = forms.IntegerField(required=True)
+    archivo = forms.FileField(required=True)
+    
+
     class Meta:
         model = Documento
-        fields = '__all__'
+        fields = ['owner','emision','proyecto']
 
 class RevisionForm(forms.ModelForm):
+    tipo = forms.IntegerField( required=True)
+    estado_cliente = forms.IntegerField(required=True)
+    estado_contratista = forms.IntegerField(required=True)
+    emitida_para = forms.CharField(widget=forms.Textarea, max_length=150, required=True)
+    fecha = forms.DateField(required=True)
+    
     class Meta:
         model = Revision
-        fields = '__all__'
+        fields = ['owner']
