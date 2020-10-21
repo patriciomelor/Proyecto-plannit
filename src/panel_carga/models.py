66b2ce7f796ef4d9dd9d01fb6884c9d6d7d0b13d
1Leo-San1 <39392131+1Leo-San1@users.noreply.github.com>
2020-10-21 11:52:16 -0300

    migracion lista donde corresponde

--- src/panel_carga/models.py
+++ src/panel_carga/models.py
@@ -70,12 +70,3 @@ class Historial(models.Model):
     def __str__(self):
         return self.fecha
 
-class Paquete(models.Model):
-    documento = models.ManyToManyField(Documento, through='PaqueteDocumento') #Relacion muchos a muchos, se redirecciona a la tabla auxiliar que se indica acá de otra manera no se podrian agregar varias veces los documentos, si bien se podria agregar 2 o mas veces el mismo documento, desconozco si se puede para varios proyectos el mismo documento.
-    nombre = models.CharField(verbose_name="Nombre del paquete", max_length=100, blank=False)
-    fecha = models.DateField(verbose_name="Fecha de respuesta")
-
-class PaqueteDocumento(models.Model): #Tabla auxiliar que basicamente es lo mismo que crea automaticamente django para la realizacion de un many to many, pero customizable a lo que necesitemos, cosa que mas adelante si necesitamos almacenar otra informacion del registro de los paquetes, se puede hacer
-    documento_id = models.ForeignKey(Documento, on_delete=models.CASCADE)
-    paquete_id = models.ForeignKey(Paquete, on_delete=models.CASCADE)
-    cantidad = models.IntegerField(default=1)
