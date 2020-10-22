a882a427eac57d403a5584e4e6937943be58e8ef
1Leo-San1 <39392131+1Leo-San1@users.noreply.github.com>
2020-10-21 23:33:18 -0300

    .

--- src/bandeja_es/views.py
+++ src/bandeja_es/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+    # OPCION 2 (MAS FACTIBLE)
+    # select Documento.num_docuemto, Documento.nombre, Documento.owner, Revision.estado_contratista, Historial.fecha
+    # from Docuemto, Revision, Historial
+    # join revision on Documento.pk = revision.Documento
+    # when (documento.pk == Historial.documento)
