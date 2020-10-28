22acf6088f51100461ea9f2fb74d280a1538a80b
Patricio Melo Riquelme <Pmeri@pmelo.Legion>
2020-10-28 12:07:50 -0300

    Revert "xd"
    This reverts commit 60afd51b76e640037dcf939d0cae0547ce05c0d0.

--- src/panel_carga/filters.py
+++
@@ -1,6 +0,0 @@
-import django_filters
-from .models import Documento
-class DocFilter(django_filters.FilterSet):
-    class Meta:
-        model = Documento
-        fields = ['especialidad', 'descripcion', 'num_documento']
