297564b3f9f9b83b9c123d7e405dbcf8c011d290
Ignaciovaldebenito <ignacio.valdebenitoa@utem.cl>
2020-12-14 00:57:52 -0300

    commit

--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from dashboard.models import User
 
-admin.site.register(User)
+#admin.site.register(User)
 
 #admin.site.register(Account)
 # Register your models here.
