a65ab29f52d0fb3a9c2446867669d0c141f21391
Pato-Console <pmelor@stod.cl>
2020-12-13 21:37:55 -0300

    Revert "Cambios de usuarios en realaciones"
    This reverts commit 875c96af1c4628dcdd25eb50bf000fb4e8205695.

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Usuario(AbstractUser):
 
-    image =models.ImageField(upload_to='users/%Y/%m/%d', null=True, blanck=True)
+    image =models.ImageField(upload_to='users/%Y/%m/%d', null=True, blank=True)
 
     def get_image(self):
         if self.image:
