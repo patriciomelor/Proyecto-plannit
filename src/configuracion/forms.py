d5cabcaab63f3635cc9c08caa9fe4ffe3e085cd7
gringows <esteban.ams95@gmail.com>
2020-12-16 20:04:07 -0300

    avance con configuracion

--- src/configuracion/forms.py
+++ src/configuracion/forms.py
@@ -17,3 +17,4 @@ class CrearUsuario(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
+
--- src/configuracion/urls.py
+++ src/configuracion/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path, include
 from . import views
+
+urlpatterns = [
+    path('nuevo_usuario/', views.UsuarioView.as_view(), name='crear-usuario'),
+]
