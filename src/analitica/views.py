2655a579c9887081e24e5f9aaca4a047213409c6
1Leo-San1 <l.snchezcasanova@uandresbello.edu>
2020-12-28 18:11:33 -0300

    grafico uno funcionando

--- src/analitica/views.py
+++ src/analitica/views.py
@@ -11,26 +11,37 @@ from bandeja_es.models import Version
 
 class IndexAnalitica(ProyectoMixin, TemplateView):
     template_name =  'analitica/index.html'
- 
-    def get_report_states(self):
-        data = []
+
+    def get_report_states_AcC(self):
         version_aprobadocCs = Version.objects.filter(estado_cliente = 1).count() #Aprobado con comentarios
+        return version_aprobadocCs
+
+    def get_report_states_Rec(self):
         version_rechazados = Version.objects.filter(estado_cliente = 2).count() #Rechazado
+        return version_rechazados
+
+    def get_report_states_Eli(self):
         version_eliminados = Version.objects.filter(estado_cliente = 3).count() #Eliminado
+        return version_eliminados
+        
+    def get_report_states_Apr(self):
         version_aprobados = Version.objects.filter(estado_cliente = 4).count() #Aprobado
+        return version_aprobados
+
+    def get_report_states_VcC(self):
         version_validoConss = Version.objects.filter(estado_cliente = 5).count() #Valido para construcción
-        version_aprobadoNums = Version.objects.filter(estado_cliente = 6).count() #Aproba en numeros
-        data = [version_aprobadocCs,version_rechazados,version_eliminados,version_aprobados,version_validoConss,version_aprobadoNums]
-        # data.append(version_aprobadocCs)
-        # data.append(version_rechazados)
-        # data.append(version_eliminados)
-        # data.append(version_aprobados)
-        # data.append(version_validoConss)
-        # data.append(version_aprobadoNums)
-        #data = [AprobadocC, Rechazado, Eliminado, Aprobado, ValidoCons, AprobadoNum]
-        return data
+        return version_validoConss
+
+    def get_report_states_ANu(self):
+        version_aprobadoNums = Version.objects.filter(estado_cliente = 6).count() #Aprobado en número
+        return version_aprobadoNums
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['report_states'] = self.get_report_states()
+        context['get_report_states_AcC'] = self.get_report_states_AcC()
+        context['get_report_states_Rec'] = self.get_report_states_Rec()
+        context['get_report_states_Eli'] = self.get_report_states_Eli()
+        context['get_report_states_Apr'] = self.get_report_states_Apr()
+        context['get_report_states_VcC'] = self.get_report_states_VcC()
+        context['get_report_states_ANu'] = self.get_report_states_ANu()
         return context
