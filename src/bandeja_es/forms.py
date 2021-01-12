d5aaab452e604cc17a0384695c181f1c8e980438
Pato-Console <pmelor@stod.cl>
2021-01-11 21:54:40 -0300

    a la ctmre wea loca

--- src/bandeja_es/forms.py
+++ src/bandeja_es/forms.py
@@ -92,7 +92,7 @@ class PaquetePreviewForm(forms.ModelForm):
         }
 class VersionDocPreview(forms.ModelForm):
     prev_revision = forms.ChoiceField(choices=TYPES_REVISION, label='Revisión')
-    prev_documento_fk = forms.CharField(label="Documentos",widget=Documento_Select2(attrs={'class': 'select2', 'id': 'formset'}))
+    prev_documento_fk = forms.CharField(label="Documentos",widget=Documento_Select2(attrs={'class': 'select2'},data_view='data_url'))
     class Meta:
         model = PrevVersion
         fields = ['prev_documento_fk', 'prev_revision', 'prev_archivo','prev_comentario' ,'prev_estado_cliente', 'prev_estado_contratista']
