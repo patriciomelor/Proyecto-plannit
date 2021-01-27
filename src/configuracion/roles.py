7738a3f0137bd26b237a908877b30a94dfb22609
1Leo-San1 <39392131+1Leo-San1@users.noreply.github.com>
2021-01-26 22:18:12 -0300

    .

--- src/configuracion/roles.py
+++ src/configuracion/roles.py
@@ -1,6 +1,8 @@
 ROLES = (
     ('','-----'),
     (1, 'Administrador'), #Acceso total
+
+    #Sin panel de configuracion
     (2, 'Revisor'), #Tiene acceso al panel de carga y Baes pero con aprobacion de su jefe o administrador
     (3, 'Vizualizador'), #Tiene acceso a solo Analitica buscador y status
     (4, 'Administrador (invitado)') #No puede configurar la plataforma
