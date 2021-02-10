8540cfed61d0a600866557c7f71be7e749d6ee9b
gringows <esteban.ams95@gmail.com>
2021-02-10 02:06:20 -0300

    avance no terminado de descarga .zip

--- src/bandeja_es/views.py
+++ src/bandeja_es/views.py
@@ -1,5 +1,7 @@
 import pathlib
 import os.path
+import zipfile
+from io import StringIO
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
@@ -68,12 +70,42 @@ class PaqueteDetail(ProyectoMixin, DetailView):
     context_object_name = 'paquete'
 
     def get_context_data(self, **kwargs):
+        listado_versiones_url = []
         context = super().get_context_data(**kwargs)
         paquete = Paquete.objects.get(pk=self.kwargs['pk'])
         versiones = PaqueteDocumento.objects.filter(paquete=paquete)
+        # for version in versiones:
+        #     act_version = version.version
+        #     static = '/static'+act_version.archivo.url
+        #     listado_versiones_url.append(static)
+        #     print(listado_versiones_url)
+
         context['versiones'] = versiones
         return context
+    
+    def post(self, request, *args, **kwargs):
+        # Sacado de https://stackoverflow.com/questions/12881294/django-create-a-zip-of-multiple-files-and-make-it-downloadable
+        listado_versiones_url = []
+        paquete = Paquete.objects.get(pk=self.kwargs['pk'])
+        versiones = PaqueteDocumento.objects.filter(paquete=paquete)
+        for version in versiones:
+            act_version = version.version
+            # static = '/static'+act_version.archivo.url
+            static = act_version.archivo.url
+            listado_versiones_url.append(static)
+        zip_subdir = "versiones"
+        zip_filename = "%s.zip" % zip_subdir
+        s = StringIO()
+        zf = zipfile.ZipFile(s, "w")
+        for fpath in listado_versiones_url:
+            fdir, fname = os.path.split(fpath)
+            zip_path = os.path.join(zip_subdir, fname)
+            zf.write(fpath, zip_path)
+        zf.close()
+        response = HttpResponse(s.getvalue(), mimetype="application/x-zip-compressed")
+        response['Content-Disposition'] = 'attachment; filename=%s' % zip_filename
 
+        return response
 class PaqueteUpdate(ProyectoMixin, UpdateView):
     model = Paquete
     template_name = 'bandeja_es/paquete-update.html'
