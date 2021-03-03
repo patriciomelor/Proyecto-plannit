8f0fb918caa64bd5c7f11124d1bbfdb1b4e619c9
gringows <esteban.ams95@gmail.com>
2021-03-03 19:15:09 -0300

    accesso para media files

--- src/dmp/urls.py
+++ src/dmp/urls.py
@@ -29,3 +29,6 @@ urlpatterns = [
     path('configuracion/', include('configuracion.urls')),
     path('buscador/', include('buscador.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
