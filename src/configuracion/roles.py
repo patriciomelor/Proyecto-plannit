b326988c09fe11e43271a58b0f52af491b255637
Pato-Console <pmelor@stod.cl>
2021-04-23 20:09:50 -0400

    roles modificados

--- src/configuracion/roles.py
+++ src/configuracion/roles.py
@@ -1,9 +1,10 @@
 ROLES = (
     ('','-----'),
-    (1, 'Administrador'), #Acceso total
-
-    #Sin panel de configuracion
-    (2, 'Revisor'), #Tiene acceso al panel de carga y Baes pero con aprobacion de su jefe o administrador
-    (3, 'Vizualizador'), #Tiene acceso a solo Analitica buscador y status
-    (4, 'Administrador (invitado)') #No puede configurar la plataforma
+    (1, 'Administrador Cliente'),     
+    (2, 'Revisor Cliente'), #Tiene acceso al panel de carga y Baes pero con aprobacion de su jefe o administrador
+    (3, 'Vizualizador Cliente'), #Tiene acceso a solo Analitica buscador y status
+ ##################################################################################
+    (4, 'Administrador Contratista'), #No puede configurar la plataforma
+    (5, 'Revisor Contratista'),
+    (6, 'Vizualizador Contratista'),
 )
