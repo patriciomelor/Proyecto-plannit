5e93da3317485fe2008f767c2da28a451a76f734
Pato-Console <pmelor@stod.cl>
2021-06-14 21:19:03 -0400

    cambios varios

--- src/bandeja_es/admin.py
+++ src/bandeja_es/admin.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from .models import Paquete
-from .models import Version
-from .models import PaqueteDocumento
-#import borradores
-from .models import BorradorPaquete
-#import preview
-from .models import PrevVersion
-from .models import PrevPaquete
-from .models import PrevPaqueteDocumento
-# Register your models here.
-
-
-
-admin.site.register(Paquete)
-admin.site.register(Version)
-admin.site.register(PaqueteDocumento)
-#Registrar Borradores
-admin.site.register(BorradorPaquete)
-#Register previw
-admin.site.register(PrevVersion)
-admin.site.register(PrevPaquete)
-admin.site.register(PrevPaqueteDocumento)
-=======
 from django.contrib import admin
 from .models import Paquete
 from .models import Version
@@ -46,4 +21,3 @@ admin.site.register(BorradorPaquete)
 admin.site.register(PrevVersion)
 admin.site.register(PrevPaquete)
 admin.site.register(PrevPaqueteDocumento)
->>>>>>> dmp-beta
