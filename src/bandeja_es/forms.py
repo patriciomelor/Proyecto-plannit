59809726b3fb8f9f3a550f761a251d72805a1cbd
Pato-Console <pmelor@stod.cl>
2021-06-14 21:27:29 -0400

    Revert "Revert "Editor de texto""
    This reverts commit 5d784d4ddefcd3a7b7252521ccdadcd7ac216061.

--- src/bandeja_es/forms.py
+++ src/bandeja_es/forms.py
@@ -37,7 +37,7 @@ class BaseArticleFormSet(BaseFormSet):
 # ***********************************
 
 class CreatePaqueteForm(forms.ModelForm):
-    descripcion = forms.CharField(widget=forms.Textarea, max_length=500)
+    descripcion = forms.CharField(widget=forms.Textarea(attrs={'id':'summernote'}), max_length=500)
     class Meta:
         model = Paquete
         fields = ['destinatario', 'asunto']
@@ -68,7 +68,11 @@ class PaquetePreviewForm(forms.ModelForm):
             'prev_comentario': 'Archivo de Comentario'
         }
         widgets = {
+<<<<<<< HEAD
             'prev_descripcion': SummernoteInplaceWidget()
+=======
+            'prev_descripcion': forms.Textarea(attrs={'id':'summernote'})
+>>>>>>> parent of 5d784d4d (Revert "Editor de texto")
         }
         
     def __init__(self, **kwargs):
