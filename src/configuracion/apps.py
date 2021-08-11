785501bc37625fb83bab72e38d2a6e80ef0bc45d
gringows <esteban.ams95@gmail.com>
2021-08-10 20:57:28 -0400

    signals configurados

--- src/configuracion/apps.py
+++ src/configuracion/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class ConfiguracionConfig(AppConfig):
     name = 'configuracion'
+
+    def ready(self) -> None:
+        import configuracion.signals
+
+        return super().ready()
