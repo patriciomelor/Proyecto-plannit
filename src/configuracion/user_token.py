104ee2ac815f865f6b0c860309e3b70a4f3b9d6f
gringows <esteban.ams95@gmail.com>
2021-09-02 16:40:00 -0400

    avance confimation link and pass change

---
+++ src/configuracion/user_token.py
@@ -0,0 +1,11 @@
+from six import text_type
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.auth.models import AbstractBaseUser
+
+class AppTokenGenerator(PasswordResetTokenGenerator):
+
+    def _make_hash_value(self, user: AbstractBaseUser, timestamp: int) -> str:
+        return (text_type(user.is_active)+text_type(user.pk)+text_type(timestamp))
+
+    
+token_generator = AppTokenGenerator()
