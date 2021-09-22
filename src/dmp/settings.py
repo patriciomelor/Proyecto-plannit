665115cc96a7a4b1e3bc884635fecdd4443ddd17
gringows <esteban.ams95@gmail.com>
2021-09-22 00:07:10 -0300

    cambios en las config y umbrales OK

--- src/dmp/settings.py
+++ src/dmp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "km6#3j^s+5kd-mq+#nbse$&_k=ee=#2$f$5in)v4d&x=dt*!=8")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "1"
