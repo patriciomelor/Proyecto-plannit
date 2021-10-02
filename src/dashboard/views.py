c1bf334d9f6c8a7474241a51df084d6884f413c8
Ignaciovaldebenito <ignacio.valdebenitoa@utem.cl>
2021-10-02 18:43:29 -0300

    evitación de problemas

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -42,7 +42,7 @@ class EscritorioView(ProyectoMixin, TemplateView):
     def get_versiones(self):
         user_roles = [1,2,4,5]
         qs1 = self.get_queryset()
-        qs2 = Version.objects.select_related('documento_fk').filter(documento_fk__in=qs1, owner__perfil__rol_usuario__in=user_roles) #.select_related("owner").filter(owner__in=users)
+        qs2 = Version.objects.select_related('documento_fk').filter(documento_fk__in=qs1, owner__perfil__rol_usuario__in=user_roles).order_by('fecha') #.select_related("owner").filter(owner__in=users)
         return qs2
 
     def get_versiones_last(self):
