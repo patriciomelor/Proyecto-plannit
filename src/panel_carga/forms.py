cdfccace6598e29e2a645b85e963dcfc6be7c39b
Pato Melo <pmelor@stod.cl>
2021-12-21 12:32:51 -0300

    codigo documento update

--- src/panel_carga/forms.py
+++ src/panel_carga/forms.py
@@ -37,10 +37,9 @@ class ProyectoForm(forms.ModelForm):
         }
 
 class DocumentoForm(forms.ModelForm):  
-
     class Meta:
         model = Documento
-        exclude = ['tipo','owner','emision','proyecto', 'ultima_edicion', 'archivo', 'Numero_documento_interno', 'Codigo_documento']
+        exclude = ['tipo','owner','emision','proyecto', 'ultima_edicion', 'archivo', 'Numero_documento_interno']
         widgets = {
             'fecha_Emision_B':forms.TextInput(attrs={'type': 'datetime','class':'form-control datepicker','placeholder':'DD/MM/YYYY'}),
             'fecha_Emision_0':forms.TextInput(attrs={'type': 'datetime','class':'form-control datepicker','placeholder':'DD/MM/YYYY'}),
